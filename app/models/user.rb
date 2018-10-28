ef11928dcface719161deca8ffdfffd9a8d46c31
森亮太 <moriryouta@moriryoutanoMacBook-ea.local>
2018-10-28 18:33:00 +0900

    更新3

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { in: 2..10 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
-  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{2,5}+\z/i
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{2,10}+\z/i
   validates :password, presence: true, format: { with: VALID_PASSWORD_REGEX }
 
 
