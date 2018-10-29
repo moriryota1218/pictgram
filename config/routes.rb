73127de5b25fad326260df68866a6eb115337e0b
森亮太 <moriryouta@moriryoutanoMacBook-ea.local>
2018-10-29 22:54:13 +0900

    更新4

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
   get 'sessions/new'
+
   root 'pages#index'
   get 'pages/help'
 
-  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+
+  resources :users
+  resources :topics
 end
