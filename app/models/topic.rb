3165729742231a06b2692593234f03b5a6913a7a
森亮太 <moriryouta@moriryoutanoMacBook-ea.local>
2018-10-30 18:52:46 +0900

    課題1

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,7 +1,8 @@
 class Topic < ApplicationRecord
   validates :user_id,presence: true
   validates :description, presence: true
-  validates :image, presence: true
+  validates :image, presence: true, size: {less_than_or_equal_to: 10.megabytes }
+  validates :image, type: {allow:['image/jpeg', 'image/jpg', 'image/png']}
 
   belongs_to :user
 
